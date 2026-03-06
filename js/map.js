6805ff332658667aa95162b5c9d8cec37f1fc9c8
Cyprien D <cyprien.deloget@sorbonne-universite.fr>
2026-03-06 15:00:54 +0100

    zoom france

--- js/map.js
+++ js/map.js
@@ -8,6 +8,8 @@ const lightgray = "#cccccc"
 const grismediations = "#444444";
 const jaunemediations = "#F2BE34";
 const rougemediations = "#D72631";
+//projections
+const lambert93 = "+proj=lcc +lat_1=49 +lat_2=44 +lat_0=46.5 +lon_0=3 +x_0=700000 +y_0=6600000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs";
 
 ///////////////////////// FONCTIONS DE CHARGEMENT DES CARTES //////////////////////////
 gettooltip = (d) => {
@@ -21,8 +23,8 @@ function loadbasemap(basedata, dataterrains) {
             params:{
                 background: "#f5f5f5",
                 container: '#carto',
-                width: 800,
-                height: 600,
+                width: window.innerWidth * 0.75,
+                height: window.innerHeight * 0.95,
                 projection: "Bertin1953",
             },
 
@@ -37,14 +39,13 @@ function loadbasemap(basedata, dataterrains) {
                     fontStyle:["bold", "italic","normal", "normal"]
                 },
 
-                
                 stroke: '#ffffff',
                 fill: {
                     type:"typo",
                     values:"TYPE_TERRAIN",
                     colors: [lightgray, jaunemediations, rougemediations],
                     defaultColor: grismediations,
-                    leg_x:550,
+                    leg_x:window.innerWidth * 0.5,
                     leg_y:20,
                     leg_title:"Type de terrain de recherche"            
                 },
@@ -59,6 +60,46 @@ function loadbasemap(basedata, dataterrains) {
                 ]
         });
 };
+
+function loadfrance(basefrance, dataterrains) {
+    return bertin.draw({
+        params:{
+            projection: lambert93,
+            background: "#f5f5f5",
+            container: '#carto-france',
+            extent: [ [-5.5, 41], [9.5, 51] ], //étendue géographique de la France métropolitaine
+        },
+        
+        layers:[
+                {type:"simple",
+                id: "terrains", //permet de cibler ce groupe pour les événements de clic
+                geojson: dataterrains,
+                symbol_size: 500,
+                tooltip: {
+                    fields:["$PRENOM_NOM", "$STATUT","-  -  -  -  -  -   cliquer pour voir le profil","$TYPE_TERRAIN"],
+                    fontSize: [40, 24, 16, 24],
+                    fontStyle:["bold", "italic","normal", "normal"]
+                },
+
+                stroke: '#ffffff',
+                fill: {
+                    type:"typo",
+                    values:"TYPE_TERRAIN",
+                    colors: [lightgray, jaunemediations, rougemediations],
+                    defaultColor: grismediations    
+                },
+               },
+
+            {
+            type: "layer",
+            geojson: basefrance,
+            fill: "white",
+            stroke: "#888888",
+            strokeWidth: 0.7
+            },
+            {type: "scalebar", units: "metric", fontSize: 40, stroke: "#888888"}
+        ]
+    })}
    
 
 
@@ -72,10 +113,12 @@ fetch('https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json')
     .catch(error => console.error('Error loading world data:', error))
     .then(data => { 
         document.getElementById("carto").appendChild( //à la suite de la div carto
-            loadbasemap(data, dataterrains) // on lance la fonction qui dessine la carte
-        )
-        
-        }).then(() => { //une fois fait, on ajoute un événement de clic sur le groupe svg terrains pour ouvrir la page du membre correspondant
+            loadbasemap(data, dataterrains)); // on lance la fonction qui dessine la carte
+
+        document.getElementById("carto-france").appendChild( //à la suite de la div carto-france
+          loadfrance(data, dataterrains));
+    })
+    .then(() => { //une fois fait, on ajoute un événement de clic sur le groupe svg terrains pour ouvrir la page du membre correspondant
 
             d3.selectAll(".terrains path")
                 .on("click", function(event, d) {
@@ -88,3 +131,4 @@ fetch('https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json')
                 })
 
     });
+
